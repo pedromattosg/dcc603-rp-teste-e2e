48acc7c5bd50168b60ba55cf8f16390334e56ce4
Pedro Henrique de Mattos Gomes <pedromattosg@hotmail.com>
2025-06-20 03:05:45 -0300

    Novo teste 01: limpa todas as tarefas completas

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,22 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Limpa todas as tarefas completas', () => {
+    cy.visit(''); 
+
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+
+    cy.get('[data-cy=todos-list] > li [data-cy=toggle-todo-checkbox]')
+      .click({ multiple: true });
+     
+    cy.get('[class=clear-completed]')
+      .click();
+
+    cy.get('[data-cy=todos-list] > li')
+      .should('have.length', 0)
+  });
+  
 });
